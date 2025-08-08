fff6cc9a28ba7a9225bf05a3ca929db05a79f522
Mohammed Esmail <143602817+hamood07@users.noreply.github.com>
2025-08-08 23:33:52 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e  # stop if any error
+
+echo "📄 Creating index.html..."
+echo "<h1>Hello from Jenkins at $(date)</h1>" > index.html
+
+echo "📦 Starting Apache container..."
+docker run -d --name mysite -p 8080:80 -v $(pwd)/index.html:/usr/local/apache2/htdocs/index.html httpd:latest
+
+echo "✅ Website running at http://localhost:8080"
